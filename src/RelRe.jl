81ca3201b868e00a422eca1453d97a3a6734d812
Kimihito Ito <kimihito.ito@gmail.com>
2023-01-16 16:53:30 +0900

    Enable multiple windows analysis

--- src/RelRe.jl
+++ src/RelRe.jl
@@ -29,10 +29,10 @@ s = ArgParseSettings()
     "--len", "-l"
     arg_type = Int64
     default = 16 # use 7 for flu
-    "--alpha", "-a"
+    "--alpha", "-a"      # TODO: estimate from mean and var
     arg_type = Float64
     default = 2.03 # use 4.5 for flu
-    "--theta", "-t"
+    "--theta", "-t"      # TODO: estimate from mean and var
     arg_type = Float64
     default = 2.32 # use 0.60 for flu
     "--unit", "-u"
@@ -41,7 +41,7 @@ s = ArgParseSettings()
     required = true
     "--estimate_GT", "-g"
     action = :store_true
-    "count_file"
+    "--count_file", "-c"
     arg_type = String
     required = true
 end
@@ -93,19 +93,20 @@ end
 
 #Renewal model of variant requencies
 function model_q(vec_c::Vector{Float64}, vec_k::Vector{Float64},
-                 vec_qt::Vector{Float64}, vec_t::Vector{Date}, t_s, t_e)
+                 vec_qt::Vector{Float64}, vec_t::Vector{Date},
+                 vec_w::Vector{Date},t_s, t_e)
     num = length(vec_c)
-    @assert length(vec_k) == num
+    @assert length(vec_k) == num * (length(vec_w) + 1)
     @assert length(vec_qt) == num
     @assert length(vec_t) == num
     
     duration = (t_e - t_s).value + 1
 
-    g = Matrix{Float64}(undef, l, num + 1)
+    g = Matrix{Float64}(undef, l, num + 1) # todo: l should be passed in args
     for j in 1:num
         g[:,j] = pmf_g(vec_c[j])
     end
-    g[:,length(vec_c)+1] = pmf_g(1.0)
+    g[: , num + 1] = pmf_g(1.0)
     
     q = Matrix{Float64}(undef, duration, num + 1)
     for j in 1:num
@@ -118,22 +119,32 @@ function model_q(vec_c::Vector{Float64}, vec_k::Vector{Float64},
     sum_q_subjects =sum(q[1,1:num])
     if(sum_q_subjects > 1)
         map(j -> q[1,j] /= sum_q_subjects,1:num)
-        q[1, length(vec_k)+1] = 0
+        q[1, num + 1] = 0
     else
-        q[1, length(vec_k)+1] = 1 - sum_q_subjects
+        q[1, num + 1] = 1 - sum_q_subjects
     end
     
     vec_sum_nmr=Vector{Float64}(undef, num)
     
     for i in 2:duration
         t = t_s + Day(i-1)
+        window_index = 1
+        for w in vec_w
+            if t < w
+                break
+            else
+                window_index += 1
+            end
+        end
+        subvec_k = map(j -> vec_k[(window_index - 1) * num + j],1:num)
+        
         fill!(vec_sum_nmr, 0.0)
         sum_dnm = 0.0
         for k in 1:l
             t_k = max(1, (t - Day(k) - t_s).value + 1)
-            vec_sum_nmr += vec(g[k, 1:num]).* vec_k .* vec(q[t_k, 1:num])
+            vec_sum_nmr += vec(g[k, 1:num]).* subvec_k .* vec(q[t_k, 1:num])
             sum_dnm += g[k, num + 1] * q[t_k, num + 1] +
-                sum(vec(g[k, 1:num]).* vec_k .* vec(q[t_k, 1:num]))
+                sum(vec(g[k, 1:num]).* subvec_k .* vec(q[t_k, 1:num]))
         end
         map(j -> q[i,j] = (vec_t[j] == t) ? vec_qt[j] :
             vec_sum_nmr[j] / sum_dnm, 1:num)
@@ -185,6 +196,7 @@ else
     subjects = arg_subjects # elements need to be checked
 end
 println("\nSubject clades")
+const num_subjects = length(subjects)
 @show(subjects)
 
 dict_index = Dict{Symbol,Int64}()
@@ -224,19 +236,33 @@ println("\nTime range of analysis")
 println("Start: " * Dates.format(t_start, ISODateFormat))
 println("End: " * Dates.format(t_end, ISODateFormat))
 
-# dates of breaks should be checked
+println("Breakpoints: " *
+    join(map(d -> Dates.format(d, ISODateFormat),breaks),","))
+
+#Check the breakpoints
+for d in breaks
+    if !(d > t_start + Day(1) && d < t_end)
+        error("Breakpoints should be > t_start + 1 and < t_end")
+    end
+end
+
+const num_windows = length(breaks) + 1 
+if (num_windows > 1)
+    println("The analysis period is divided into "
+            * string(num_windows) * " windows.")
+end
 
 dates = df_count.date
 mat_obs = Matrix(filter(x->x.date in dates, df_count)[:,vcat(subjects,baseline)])
 
 function negLogL(par::Vector, grad::Vector)
-    # println(par)
-    vec_c = par[1:length(subjects)]
-    vec_k = par[length(subjects)+1:length(subjects)*2] ## should be mat_k
-    vec_qt = par[length(subjects)*2+1:length(subjects)*3]
+    vec_c = par[1:num_subjects]
+    vec_k = par[num_subjects+1:num_subjects * (num_windows + 1)]
+    vec_qt = par[num_subjects*(num_windows+1)+1:num_subjects*(num_windows+2)]
     vec_t = map(v -> dict_first[v], subjects)
+    vec_w = breaks
     try
-        q = model_q(vec_c, vec_k, vec_qt, vec_t, t_start, t_end)
+        q = model_q(vec_c, vec_k, vec_qt, vec_t, vec_w, t_start, t_end)
         sumll = 0.0
         for i in 1:length(dates)
             j = Dates.value(dates[i]-t_start)+1
@@ -247,7 +273,7 @@ function negLogL(par::Vector, grad::Vector)
             elseif(unit==:D)
                 rows = [j]
             end
-            probs = vec(max.(0, mean(q[rows, 1:length(subjects)+1], dims=1)))
+            probs = vec(max.(0, mean(q[rows, 1:num_subjects+1], dims=1)))
             obs = mat_obs[i,:]
             sumll += logpdf(Multinomial(sum(obs), probs), obs)
         end
@@ -257,29 +283,29 @@ function negLogL(par::Vector, grad::Vector)
         return -sumll
     catch e
         println(e)
-        throw(e)
+        eixt(1)
     end
 end
 
-vec_c_start = fill(1.0,length(subjects))
+vec_c_start = fill(1.0,num_subjects)
 if estimate_GT
     if unit == :D
-        vec_c_lb = fill(1.0e-10,length(subjects))
-        vec_c_ub = fill(10.0,length(subjects))
+        vec_c_lb = fill(1.0e-10,num_subjects)
+        vec_c_ub = fill(10.0,num_subjects)
     else
         println("error: The unit should be a day for the -g option")
         exit(1)
     end
 else
-    vec_c_lb = fill(1.0,length(subjects))
-    vec_c_ub = fill(1.0,length(subjects))
+    vec_c_lb = fill(1.0,num_subjects)
+    vec_c_ub = fill(1.0,num_subjects)
 end
-vec_k_start = fill(1.0,length(subjects))
-vec_k_lb = fill(1.0e-10,length(subjects))
-vec_k_ub = fill(10.0,length(subjects))
-vec_qt_start = fill(0.001,length(subjects))
-vec_qt_lb = fill(1.0e-10,length(subjects))
-vec_qt_ub = fill(1.0,length(subjects))
+vec_k_start = fill(1.0,num_subjects * num_windows)
+vec_k_lb = fill(1.0e-10,num_subjects * num_windows)
+vec_k_ub = fill(10.0,num_subjects * num_windows)
+vec_qt_start = fill(0.001,num_subjects)
+vec_qt_lb = fill(1.0e-10,num_subjects)
+vec_qt_ub = fill(1.0,num_subjects)
 
 par_start = vcat(vec_c_start, vec_k_start, vec_qt_start)
 par_lb = vcat(vec_c_lb, vec_k_lb, vec_qt_lb)
@@ -296,13 +322,22 @@ println("Maximizing the likehood function")
 nmaxll, par_maxll, err = optimize(opt, par_start)
 println("Maximization finished")
 
-num = length(subjects)
+println(par_maxll)
 
 df_ml = DataFrame()
 map(x -> df_ml[!,"t_" * string(x)] = [dict_first[x]],subjects)
 map(x -> df_ml[!,"c_" * string(x)] = [par_maxll[dict_index[x]]], subjects)
-map(x -> df_ml[!,"k_" * string(x)] = [par_maxll[dict_index[x]+num]], subjects)
-map(x -> df_ml[!,"qt_"* string(x)] = [par_maxll[dict_index[x]+num*2]], subjects)
+if num_windows > 1
+    for w in 1:num_windows
+        map(x -> df_ml[!,"k_" * string(x) * "_w" * string(w)]
+            = [par_maxll[num_subjects * w + dict_index[x]]], subjects)
+    end
+else
+    map(x -> df_ml[!,"k_" * string(x)]
+        = [par_maxll[num_subjects + dict_index[x]]], subjects)
+end
+map(x -> df_ml[!,"qt_"* string(x)]
+    = [par_maxll[num_subjects * (num_windows + 1) + dict_index[x]]], subjects)
 df_ml[!,"maxll"] = [-nmaxll]
 df_ml[!,"convergence"] = [err]
 CSV.write("estimates_ml.csv", df_ml)
@@ -327,11 +362,13 @@ function f2(par, grad,i)
     -par[i]
 end
 
-mat_par_95 = Matrix{Float64}(undef, length(subjects)*6, length(subjects)*3)
+mat_par_95 = Matrix{Float64}(undef,
+                             num_subjects * (2 + 2 * num_windows + 2),
+                             num_subjects * (1 + 1 * num_windows + 1))
 
 println("Calculating 95% confidence intervals (CIs)")
 
-Threads.@threads for i in 1:length(subjects)
+Threads.@threads for i in 1:num_subjects
     println("Thread " * string(Threads.threadid()) * " calculates CIs of " *
         string(subjects[i]))
     
@@ -344,40 +381,56 @@ Threads.@threads for i in 1:length(subjects)
     inequality_constraint!(opt_c, (par,grad) -> constr(par,grad), 1e-6)
     
     opt_l = NLopt.Opt(:LN_SBPLX, length(par_maxll))
-    opt_l.xtol_rel = 1e-6
+    opt_l.xtol_rel = 1e-6 #todo precision
     opt_c.local_optimizer = opt_l
 
+    # Lower bound of vec_c[i]
     opt_c.min_objective = (par, grad) -> f1(par, grad, i)
     lb, par_95, err_95 = optimize(opt_c, par_maxll)
     mat_par_95[i, :] = par_95
-    
+    # Upper bound of vec_c[i]
     opt_c.min_objective = (par, grad) -> f2(par, grad, i)
     ub, par_95, err_95 = optimize(opt_c, par_maxll)
-    mat_par_95[length(subjects) + i, :] = par_95
+    mat_par_95[num_subjects + i, :] = par_95
+    for j in 1:num_windows
+        # Lower bound of vec_k[i] for jth window
+        opt_c.min_objective = (par, grad) ->
+            f1(par, grad, j * num_subjects + i)
+        lb, par_95, err_95 = optimize(opt_c, par_maxll)
+        mat_par_95[num_subjects * 2 + num_subjects*2*(j-1)+2*i-1,:] = par_95
 
-    opt_c.min_objective = (par, grad) -> f1(par, grad, num+i)
+        # Upper bound of vec_k[i] for jth window        
+        opt_c.min_objective = (par, grad) ->
+            f2(par, grad, j * num_subjects + i)
+        ub, par_95, err_95 = optimize(opt_c, par_maxll)
+        mat_par_95[num_subjects * 2 + num_subjects*2*(j-1)+2*i, :] = par_95
+    end
+    # Lower bound of vec_qt_[i]    
+    opt_c.min_objective = (par, grad) ->
+        f1(par, grad, num_subjects * (num_windows + 1) +i)
     lb, par_95, err_95 = optimize(opt_c, par_maxll)
-    mat_par_95[2*length(subjects) + i, :] = par_95
-
-    opt_c.min_objective = (par, grad) -> f2(par, grad, num+i)
+    mat_par_95[num_subjects * 2 + num_subjects * num_windows * 2 + i, :] = par_95
+    # Upper bound of vec_qt_[i]
+    opt_c.min_objective = (par, grad) ->
+        f2(par, grad, num_subjects * (num_windows + 1) +i)
     ub, par_95, err_95 = optimize(opt_c, par_maxll)
-    mat_par_95[3*length(subjects) + i, :] = par_95
-
-    opt_c.min_objective = (par, grad) -> f1(par, grad, 2*num+i)
-    lb, par_95, err_95 = optimize(opt_c, par_maxll)
-    mat_par_95[4*length(subjects) + i, :] = par_95
-
-    opt_c.min_objective = (par, grad) -> f2(par, grad, 2*num+i)
-    ub, par_95, err_95 = optimize(opt_c, par_maxll)
-    mat_par_95[5*length(subjects) + i, :] = par_95
-    
+    mat_par_95[num_subjects * 2 + num_subjects * num_windows * 2 + num_subjects + i, :] = par_95
     println("Calculation for CIs of " * string(subjects[i]) * " finished")
 end
 
 df_95 = DataFrame()
-map(x -> df_95[!,"t_" * string(x)] = repeat([dict_first[x]],length(subjects)*6),subjects)
+map(x -> df_95[!,"t_" * string(x)] = repeat([dict_first[x]],num_subjects * (2 + 2 * num_windows + 2)),subjects)
 map(x -> df_95[!,"c_" * string(x)] = mat_par_95[:,dict_index[x]], subjects)
-map(x -> df_95[!,"k_" * string(x)] = mat_par_95[:,dict_index[x]+num], subjects)
-map(x -> df_95[!,"qt_"* string(x)] = mat_par_95[:,dict_index[x]+num*2], subjects)
+
+if num_windows > 1
+    for w in 1:num_windows
+        map(x -> df_95[!,"k_" * string(x) * "_w" * string(w)]
+            = mat_par_95[:,dict_index[x] + num_subjects * w],subjects)
+    end
+else
+    map(x -> df_95[!,"k_" * string(x)]
+        = mat_par_95[:,dict_index[x]+num_subjects],subjects)
+end
+map(x -> df_95[!,"qt_"* string(x)] = mat_par_95[:,dict_index[x]+num_subjects*(num_windows+1)], subjects)
 CSV.write("estimates_95CI.csv", df_95)
 
