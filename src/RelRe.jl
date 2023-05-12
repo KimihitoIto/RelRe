14009269fb6db3163ec00e4206a9bff508d0736e
Kimihito Ito <kimihito.ito@gmail.com>
2023-05-12 15:35:09 +0900

    Drop the breaking windows functionality

--- src/RelRe.jl
+++ src/RelRe.jl
@@ -28,10 +28,6 @@ s = ArgParseSettings()
     arg_type = Symbol
     nargs = '*'
     default = []
-    "--breaks", "-w"
-    arg_type = Date
-    nargs = '*'
-    default = []
     "--precision", "-p"
     arg_type = Float64
     default = 1e-4
@@ -76,7 +72,6 @@ const len_tr = parsed_args["len"]
 const alpha = parsed_args["alpha"]
 const theta = parsed_args["theta"]
 const unit = parsed_args["unit"]
-const breaks = parsed_args["breaks"]
 const arg_subjects = parsed_args["subjects"]
 const estimate_GT = parsed_args["estimate_GT"]
 const estimate_CI = parsed_args["estimate_CI"]
@@ -102,9 +97,9 @@ end
 #Renewal model of variant requencies
 function model_q(vec_c::Vector{Float64}, vec_k::Vector{Float64},
                  vec_qt::Vector{Float64}, vec_t::Vector{Date},
-                 vec_b::Vector{Date},t_s, t_e, l)
+                 t_s, t_e, l)
     @assert length(vec_c) == num_subjects
-    @assert length(vec_k) == num_subjects * (length(vec_b) + 1)
+    @assert length(vec_k) == num_subjects
     @assert length(vec_qt) == num_subjects
     @assert length(vec_t) == num_subjects
     
@@ -137,23 +132,14 @@ function model_q(vec_c::Vector{Float64}, vec_k::Vector{Float64},
     
     for i in 2:length(delta:delta:duration)
         t = t_s + Day(Int64(floor((delta:delta:duration)[i])))
-        window_index = 1
-        for b in vec_b # the first days of next windows
-            if t < b
-                break
-            else
-                window_index += 1
-            end
-        end
-        subvec_k = map(j -> vec_k[(window_index - 1) * num_subjects + j],1:num_subjects)
         
         fill!(vec_sum_nmr, 0.0)
         sum_dnm = 0.0
         for k in 1:length(delta:delta:l)#here
             t_k = max(1, i - k)
-            vec_sum_nmr += vec(g[k, 1:num_subjects]).* subvec_k .* vec(q[t_k, 1:num_subjects])
+            vec_sum_nmr += vec(g[k, 1:num_subjects]).* vec_k .* vec(q[t_k, 1:num_subjects])
             sum_dnm += g[k, num_subjects + 1] * q[t_k, num_subjects + 1] +
-                sum(vec(g[k, 1:num_subjects]).* subvec_k .* vec(q[t_k, 1:num_subjects]))
+                sum(vec(g[k, 1:num_subjects]).* vec_k .* vec(q[t_k, 1:num_subjects]))
         end
         map(j -> q[i,j] = (vec_t[j] == t) ? vec_qt[j] :
             vec_sum_nmr[j] / sum_dnm, 1:num_subjects)
@@ -261,33 +247,17 @@ end
 println("\nTime range of analysis")
 println("Start: " * Dates.format(t_start, ISODateFormat))
 println("End: " * Dates.format(t_end, ISODateFormat))
-println("Breakpoints: [" *
-    join(map(d -> Dates.format(d, ISODateFormat),breaks),",") * "]")
-
-#Check the breakpoints
-for d in breaks
-    if !(d > t_start + Day(1) && d < t_end)
-        error("Breakpoints should be > t_start + 1 and < t_end")
-    end
-end
-
-const num_windows = length(breaks) + 1 
-if (num_windows > 1)
-    println("The analysis period was divided into "
-            * string(num_windows) * " windows.")
-end
 
 dates = df_count.date
 mat_obs = Matrix(filter(x->x.date in dates, df_count)[:,vcat(subjects,baseline)])
 
 function negLogL(par::Vector, grad::Vector)
     vec_c = par[1:num_subjects]
-    vec_k = par[num_subjects+1:num_subjects * (num_windows + 1)]
-    vec_qt = par[num_subjects*(num_windows+1)+1:num_subjects*(num_windows+2)]
+    vec_k = par[num_subjects+1:num_subjects * 2]
+    vec_qt = par[num_subjects*2+1:num_subjects*3]
     vec_t = map(v -> dict_first[v], subjects)
-    vec_b = breaks
     try
-        q = model_q(vec_c, vec_k, vec_qt, vec_t, vec_b, t_start, t_end, len_tr)
+        q = model_q(vec_c, vec_k, vec_qt, vec_t, t_start, t_end, len_tr)
         sumll = 0.0
         for i in 1:length(dates)
             j = Dates.value(dates[i]-t_start)+1
@@ -325,9 +295,9 @@ else
     vec_c_lb = fill(1.0,num_subjects)
     vec_c_ub = fill(1.0,num_subjects)
 end
-vec_k_start = fill(1.0,num_subjects * num_windows)
-vec_k_lb = fill(1.0e-10,num_subjects * num_windows)
-vec_k_ub = fill(10.0,num_subjects * num_windows)
+vec_k_start = fill(1.0,num_subjects)
+vec_k_lb = fill(1.0e-10,num_subjects)
+vec_k_ub = fill(10.0,num_subjects)
 vec_qt_start = fill(0.001,num_subjects)
 vec_qt_lb = fill(1.0e-10,num_subjects)
 vec_qt_ub = fill(1.0,num_subjects)
@@ -371,13 +341,13 @@ function f2(par, grad,i)
 end
 
 mat_95CI = Matrix{Float64}(undef,
-                           num_subjects * (2 + 2 * num_windows + 2),
-                           num_subjects * (1 + 1 * num_windows + 1))
-err_95CI = Vector{Symbol}(undef,num_subjects * (2 + 2 * num_windows + 2))
+                           num_subjects * (2 + 2 + 2),
+                           num_subjects * (1 + 1 + 1))
+err_95CI = Vector{Symbol}(undef,num_subjects * (2 + 2 + 2))
 
 if estimate_CI
     println("\nCalculating 95% confidence intervals (CIs)")
-    Threads.@threads for i in 1:(num_subjects * (2 + 2 * num_windows + 2))
+    Threads.@threads for i in 1:(num_subjects * (2 + 2 + 2))
         println("Thread " * string(Threads.threadid()) * " is working on the " *
             string(i) * " th loop of the CI calculation")
         
@@ -415,21 +385,13 @@ if estimate_CI
     df_estimates[!,"c_lb"] = Vector{Float64}()
     df_estimates[!,"c_ub"] = Vector{Float64}()
 end
-if num_windows > 1
-    for w in 1:num_windows
-        df_estimates[!,"k_w"*string(w)] = Vector{Float64}()
-        if estimate_CI
-            df_estimates[!,"k_w"*string(w)*"_lb"] = Vector{Float64}()
-            df_estimates[!,"k_w"*string(w)*"_ub"] = Vector{Float64}()
-        end
-    end
-else
-    df_estimates[!,"k"] = Vector{Float64}()
-    if estimate_CI
-        df_estimates[!,"k_lb"] = Vector{Float64}()
-        df_estimates[!,"k_ub"] = Vector{Float64}()
-    end
+
+df_estimates[!,"k"] = Vector{Float64}()
+if estimate_CI
+    df_estimates[!,"k_lb"] = Vector{Float64}()
+    df_estimates[!,"k_ub"] = Vector{Float64}()
 end
+
 df_estimates[!,"qt"] = Vector{Float64}()
 if estimate_CI
     df_estimates[!,"qt_lb"] = Vector{Float64}()
@@ -441,31 +403,16 @@ for j in 1:num_subjects
     if estimate_CI
         row = vcat(row, minimum(mat_95CI[:,j]), maximum(mat_95CI[:,j])) #lb, ub
     end
-    if num_windows > 1
-        for w in 1:num_windows
-            row=vcat(row, par_maxll[num_subjects+(w-1)*num_subjects + j])#k
-            if estimate_CI
-                row=vcat(row,
-                         minimum(mat_95CI[:,num_subjects+(w-1)*num_subjects+j]),#lb
-                         maximum(mat_95CI[:,num_subjects+(w-1)*num_subjects+j]))#ub
-            end
-        end
-        row=vcat(row, par_maxll[(1+num_windows)*num_subjects+j]) #qt
-        if estimate_CI
-            row=vcat(row, minimum(mat_95CI[:,(1+num_windows)*num_subjects+j]),#lb
-                     maximum(mat_95CI[:,(1+num_windows)*num_subjects+j]))#ub
-        end
-    else
-        row=vcat(row, par_maxll[num_subjects+j])#k
-        if estimate_CI
-            row=vcat(row, minimum(mat_95CI[:,num_subjects + j]), #lb
-                     maximum(mat_95CI[:,num_subjects + j])) #ub
-        end
-        row=vcat(row, par_maxll[2 * num_subjects+j])#qt
-        if estimate_CI
-            row=vcat(row, minimum(mat_95CI[:,2 * num_subjects + j]), #lb
-                     maximum(mat_95CI[:,2 * num_subjects + j])) #ub
-        end
+
+    row=vcat(row, par_maxll[num_subjects+j])#k
+    if estimate_CI
+        row=vcat(row, minimum(mat_95CI[:,num_subjects + j]), #lb
+                 maximum(mat_95CI[:,num_subjects + j])) #ub
+    end
+    row=vcat(row, par_maxll[2 * num_subjects+j])#qt
+    if estimate_CI
+        row=vcat(row, minimum(mat_95CI[:,2 * num_subjects + j]), #lb
+                 maximum(mat_95CI[:,2 * num_subjects + j])) #ub
     end
     push!(df_estimates,row)
 end
@@ -481,9 +428,9 @@ CSV.write(outfile_estimate, df_estimates)
 #Calculate Trajectory
 
 if estimate_GT
-    freedom = num_subjects * (1 + num_windows + 1)
+    freedom = num_subjects * (1 + 1 + 1)
 else 
-    freedom = num_subjects * (num_windows + 1)
+    freedom = num_subjects * (1 + 1)
 end
 
 function constr_trajectory(vec_par, vec_grad)
@@ -491,20 +438,19 @@ function constr_trajectory(vec_par, vec_grad)
 end
 
 mat_95CR = Matrix{Float64}(undef,
-                           num_subjects * (2 + 2 * num_windows + 2),
-                           num_subjects * (1 + 1 * num_windows + 1))
-err_95CR = Vector{Symbol}(undef,num_subjects * (2 + 2 * num_windows + 2))
+                           num_subjects * (2 + 2 + 2),
+                           num_subjects * (1 + 1 + 1))
+err_95CR = Vector{Symbol}(undef,num_subjects * (2 + 2 + 2))
 
 if calculate_q
     println("\nCalculating trajectory")
-    vec_c_ml = par_maxll[1:num_subjects] #c
-    vec_k_ml = par_maxll[num_subjects+1:num_subjects*(1+num_windows)] #k
-    vec_qt_ml = par_maxll[num_subjects*(1+num_windows)+1:num_subjects*(2+num_windows)] #qt
+    vec_c_ml = par_maxll[1 : num_subjects] #c
+    vec_k_ml = par_maxll[num_subjects+1 : num_subjects*2] #k
+    vec_qt_ml = par_maxll[num_subjects*2+1 : num_subjects*3] #qt
     vec_t_ml = map(v -> dict_first[v], subjects)
-    vec_b_ml = breaks
 
     t_future = t_end + Day(days_to_predict)
-    q_ml = model_q(vec_c_ml, vec_k_ml, vec_qt_ml, vec_t_ml, vec_b_ml,
+    q_ml = model_q(vec_c_ml, vec_k_ml, vec_qt_ml, vec_t_ml,
                    t_start, t_future, len_tr)
     q_lb = q_ml
     q_ub = q_ml
@@ -512,31 +458,13 @@ if calculate_q
     vec_average_c_lb = vec_average_c_ml
     vec_average_c_ub = vec_average_c_ml
 
-    if num_windows > 1
-        vec_average_k_ml = Vector{Float64}(undef,0)
-        row_from = 1
-        for w in 1:length(breaks)
-            local subvec_k = map(j -> vec_k_ml[(w - 1) * num_subjects + j],
-                           1:num_subjects)
-            global row_to = (breaks[w]-t_start).value
-            global vec_average_k_ml =
-                vcat(vec_average_k_ml,
-                     q_ml[row_from:row_to,:] * vcat(subvec_k, 1.0))
-            global row_from = row_to +1
-        end
-        row_to= (t_future - t_start).value + 1
-        local subvec_k = map(j -> vec_k_ml[length(breaks) * num_subjects + j],
-                             1:num_subjects)
-        vec_average_k_ml = vcat(vec_average_k_ml,
-                                q_ml[row_from:row_to,:] * vcat(subvec_k, 1.0))        else
-        vec_average_k_ml = q_ml * vcat(vec_k_ml, 1.0)
-    end
+    vec_average_k_ml = q_ml * vcat(vec_k_ml, 1.0)
     vec_average_k_lb = vec_average_k_ml
     vec_average_k_ub = vec_average_k_ml
         
     if estimate_CI
         println("Calculating 95% confidence region (CR)")
-        Threads.@threads for i in 1:(num_subjects * (2 + 2 * num_windows + 2))
+        Threads.@threads for i in 1:(num_subjects * (2 + 2 + 2))
             println("Thread " * string(Threads.threadid()) *
                 " is working on the " *
                 string(i) * " th loop of the CR calculation")
@@ -566,41 +494,17 @@ if calculate_q
         end
         println(err_95CR)
 
-        for i in 1:(num_subjects * (2 + 2 * num_windows + 2))
+        for i in 1:(num_subjects * (2 + 2 + 2))
             par_cr = mat_95CR[i, :]
-            vec_c_cr = par_cr[1:num_subjects] #c
-            vec_k_cr = par_cr[num_subjects+1:num_subjects*(1+num_windows)] #k
-            vec_qt_cr= par_cr[num_subjects*(1+num_windows)+1:num_subjects*(2+num_windows)] #qt
+            vec_c_cr = par_cr[1 : num_subjects] #c
+            vec_k_cr = par_cr[num_subjects+1 : num_subjects*2] #k
+            vec_qt_cr= par_cr[num_subjects*2+1 : num_subjects*3] #qt
             vec_t_cr = map(v -> dict_first[v], subjects)
-            vec_b_cr = breaks
 
-            q_cr = model_q(vec_c_cr, vec_k_cr, vec_qt_cr, vec_t_cr, vec_b_cr,
+            q_cr = model_q(vec_c_cr, vec_k_cr, vec_qt_cr, vec_t_cr, 
                            t_start, t_future, len_tr)
-            local vec_average_c_cr = q_cr * vcat(vec_c_cr, 1.0)
-
-            if num_windows > 1
-                vec_average_k_cr = Vector{Float64}(undef,0)
-                global row_from = 1
-                for w in 1:length(breaks)
-                    local subvec_k =
-                        map(j -> vec_k_cr[(w - 1) * num_subjects + j],
-                            1:num_subjects)
-                    global row_to = (breaks[w]-t_start).value
-                    vec_average_k_cr =
-                        vcat(vec_average_k_cr,
-                             q_cr[row_from:row_to,:] * vcat(subvec_k, 1.0))
-                    global row_from = row_to +1
-                end
-                global row_to= (t_future - t_start).value + 1
-                local subvec_k =
-                    map(j -> vec_k_cr[length(breaks) * num_subjects + j],
-                        1:num_subjects)
-                vec_average_k_cr =
-                    vcat(vec_average_k_cr,
-                         q_cr[row_from:row_to,:] * vcat(subvec_k, 1.0))
-            else
-                vec_average_k_cr = q_cr * vcat(vec_k_cr, 1.0)
-            end
+            local vec_average_c_cr = q_cr * vcat(vec_c_cr, 1.0) #local?
+            vec_average_k_cr = q_cr * vcat(vec_k_cr, 1.0)
             
             global q_lb = min.(q_lb, q_cr)
             global q_ub = max.(q_ub, q_cr)
